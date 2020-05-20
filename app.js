0b454b55b2ea32afb14985fa28d7b4b9987c6990
Hadrien Laperriere <hadrien.laperriere@gmail.com>
2020-05-20 10:32:20 +0200

    add some text styling

--- app.js
+++ app.js
@@ -13,16 +13,21 @@ function game () {
         playerWeaponImg.addEventListener('click', (e) => {
 
             // Play a round with the proper player and computer selections
-            let roundResult = playRound(e.srcElement.id, computerPlay());
+            let computerPick = computerPlay();
+            let roundResult = playRound(e.srcElement.id, computerPick);
 
+            // Trigger the animation for player
+            animationPlayerimage (e.srcElement.id);
+            animationCPUimage (`cpu${computerPick}`);
+            
             // Display the round number & round Result
             document.getElementById("roundContentFirst").textContent = `Round ${roundCount ++}`;
             document.getElementById("roundContent").lastChild.textContent = roundResult;
 
             // Update the score
-            if (roundResult.includes("You win")){
+            if (roundResult.includes("You Win")){
                 playerScore ++;
-            } else if (roundResult.includes("You loose")){
+            } else if (roundResult.includes("You Loose")){
                 computerScore ++;
             }
 
@@ -39,51 +44,50 @@ function game () {
                 } else {
                     console.log(`The final score is ${playerScore} for you and ${computerScore} for the computer, it's a tie :|`);
                 }
-        
-        
             }
-    
         });
     }); 
-
+}
     
-
-
-
-        
-
-
-
-    
-    
-  
-
-
-    
-    
-    // Computer random pick and return his choice
-    function computerPlay () {
-        switch (Math.floor(Math.random() * 3)){
-            case 0 : return "Rock";
-            case 1 : return "Paper";
-            case 2 :return "Scissors";
-        }
+// Computer random pick and return his choice
+function computerPlay () {
+    switch (Math.floor(Math.random() * 3)){
+        case 0 : return "Rock";
+        case 1 : return "Paper";
+        case 2 :return "Scissors";
     }
+}
+
+// Play a round and return the result
+function playRound(playerSelection, computerSelection) {
+    let result = null;
     
-    // Play a round and return the result
-    function playRound(playerSelection, computerSelection) {
-        let result = null;
-        
-        if (playerSelection == computerSelection){
-            result = `You both played ${computerSelection}, it's a draw!`;
-        } else if ((playerSelection == "Paper" && computerSelection == "Rock") ||
-                    (playerSelection == "Rock" && computerSelection == "Scissors") ||
-                    (playerSelection == "Scissors" && computerSelection == "Paper")) { 
-            result = `You win! ${playerSelection} beats ${computerSelection}`;      
-        } else {
-            result = `You loose! ${computerSelection} beats ${playerSelection}`;  
-        }
-        return result;
+    if (playerSelection == computerSelection){
+        result = `You both played ${computerSelection}` + "<br>" + "It's a Draw!";
+    } else if ((playerSelection == "Paper" && computerSelection == "Rock") ||
+                (playerSelection == "Rock" && computerSelection == "Scissors") ||
+                (playerSelection == "Scissors" && computerSelection == "Paper")) { 
+        result = `${playerSelection} beats ${computerSelection}` + "<br>" + 'You Win!';      
+    } else {
+        result = `${computerSelection} beats ${playerSelection}` + "<br>" + 'You Loose!';
     }
-    
+    return result;
+}
+
+// Player image animation when click
+function animationPlayerimage (imageID) {
+    const imageToAnimate = document.getElementById(`${imageID}`)
+    imageToAnimate.classList.add('playerImgAnimation');
+    imageToAnimate.addEventListener( "animationend",  function() {
+        imageToAnimate.classList.remove("playerImgAnimation");    
+    } );
+}
+
+// Computer image animation
+function animationCPUimage (imageID) {
+    const imageToAnimate = document.getElementById(`${imageID}`)
+    imageToAnimate.classList.add('computerImgAnimation');
+    imageToAnimate.addEventListener( "animationend",  function() {
+        imageToAnimate.classList.remove("computerImgAnimation");    
+    } );
 }
